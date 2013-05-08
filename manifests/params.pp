55fca77b94dbb8be6cd28826311406a20c737418
Jan Collijs <jan.collijs@inuits.eu>
2013-05-08 11:35:47 +0200

    Bugfix: declaration of environment booleans

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,24 +13,25 @@ class hubot::params {
   $log_level                     = 'DEBUG'
 
   $adapter                       = undef
+  $plugins                       = undef
 
   $irc_nickname                  = undef
   $irc_server                    = undef
   $irc_rooms                     = undef
-  $irc_debug                     = undef
-  $irc_unflood                   = undef
+  $irc_debug                     = false
+  $irc_unflood                   = false
   $irc_port                      = undef
   $irc_password                  = undef
   $irc_nickserv_password         = undef
   $irc_nickserv_username         = undef
-  $irc_server_fake_ssl           = undef
-  $irc_usessl                    = undef
+  $irc_server_fake_ssl           = false
+  $irc_usessl                    = false
 
   $xmpp_username                 = undef
   $xmpp_password                 = undef
   $xmpp_rooms                    = undef
   $xmpp_host                     = undef
   $xmpp_port                     = undef
-  $xmpp_legacyssl                = undef
+  $xmpp_legacyssl                = false
   $xmpp_preferred_sasl_mechanism = undef
 }
